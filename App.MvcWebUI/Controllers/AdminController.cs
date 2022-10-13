40974b238b191cc5b2047b83e95577ed7153c872
seyidovmurad <seyidovmurad@outlook.com>
2022-10-13 13:01:36 +0400

    Admin Panel Crud

---
+++ App.MvcWebUI/Controllers/AdminController.cs
@@ -0,0 +1,87 @@
+﻿using App.Business.Abstract;
+using App.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.MvcWebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public AdminController(IProductService productService, ICategoryService categoryService)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new ProductListViewModel
+            {
+                Products = _productService.GetAll()
+            };
+            return View(model);
+        }
+
+        public IActionResult Add()
+        {
+            var model = new AddEditProductViewModel
+            {
+                Product = new Product(),
+                Categories = _categoryService.GetAll(),
+                IsActionAdd = true
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            if(ModelState.IsValid)
+            {
+                _productService.Add(product);
+                TempData.Add("message", "Product Added Successfully");
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Update(int productId)
+        {
+            var model = new AddEditProductViewModel
+            {
+                Product = _productService.GetById(productId),
+                Categories = _categoryService.GetAll(),
+                IsActionAdd = false
+            };
+            return View("Add", model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            if(ModelState.IsValid)
+            {
+                _productService.Update(product);
+                TempData.Add("message", "Product updated successfully");
+                return RedirectToAction("Index");
+            }
+            return View("Add");
+        }
+
+        public IActionResult Delete(int productId)
+        {
+            try
+            {
+                _productService.Delete(productId);
+                TempData.Add("message", "Product deleted successfully");
+            }
+            catch
+            {
+                TempData.Add("message", "Something went wrong!");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ App.MvcWebUI/Models/AddEditProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿using App.Entities.Concrete;
+
+namespace App.MvcWebUI
+{
+    public class AddEditProductViewModel
+    {
+        public Product Product { get; set; }
+        public List<Category> Categories { get; set; }
+        public bool IsActionAdd { get; internal set; }
+    }
+}
